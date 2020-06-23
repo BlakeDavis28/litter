749ba41619b8dc77c3d11c029fe138f5bbcc7d39
BlakeDavis28 <blake_davis@outlook.com>
2020-06-23 12:56:12 +1000

    added profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+  def profile
+    @userid = params[:user_id]
+    @userinfo = User.find(params[:user_id])
+    @usercount = @userinfo.lits
+    @lits = Lit.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,8 @@ Rails.application.routes.draw do
 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    get 'profile'
+  end
+
 end
